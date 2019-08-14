8d3072fcb3960ac8754a3cb352860816b74ba144
Joshua Sharp <sharp.josh@gmail.com>
2019-08-14 15:20:51 -0400

    Layout done

---
+++ GrailDiary/GrailDiary/CluesTableViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GrailDiary
+//
+//  Created by Joshua Sharp on 8/14/19.
+//  Copyright © 2019 Joshua Sharp. All rights reserved.
+//
+
+import UIKit
+
+class CluesTableViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
