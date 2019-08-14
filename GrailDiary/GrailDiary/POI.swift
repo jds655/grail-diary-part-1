2e2b77837f97abb4ff2ff862d4a5788b7c26f59a
Joshua Sharp <sharp.josh@gmail.com>
2019-08-14 15:37:24 -0400

    Added new files

---
+++ GrailDiary/GrailDiary/POI.swift
@@ -0,0 +1,9 @@
+//
+//  POI.swift
+//  GrailDiary
+//
+//  Created by Joshua Sharp on 8/14/19.
+//  Copyright © 2019 Joshua Sharp. All rights reserved.
+//
+
+import Foundation
---
+++ GrailDiary/GrailDiary/POIDetailViewController.swift
@@ -0,0 +1,30 @@
+//
+//  POIDetailViewController.swift
+//  GrailDiary
+//
+//  Created by Joshua Sharp on 8/14/19.
+//  Copyright © 2019 Joshua Sharp. All rights reserved.
+//
+
+import UIKit
+
+class POIDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
